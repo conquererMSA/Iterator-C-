5054ffc170c81626e5d20dee030045760a3801cb
asus test <msa-99@outlook.com>
2024-06-06 00:47:58 +0600

    printSpecificIndexElement by Iterator

--- printSpecificElementOfArray.cpp
+++ printSpecificElementOfArray.cpp
@@ -1,14 +1,35 @@
-//print specific index element
+// //print specific index element
+// //int arr[5]={1,2,3,4,5}; erokom array er jonno STL e built in iterator nei. onno container data typer er jonno iterator ache.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[5]={1,2,3,4,5};
+//     int size=sizeof(arr)/sizeof(int);
+//     int userPos;
+//     cin>>userPos;
+//     int* end=arr+size;
+//     int* indexValuePointer=arr+userPos-1;
+//     if(end!=indexValuePointer){
+//         cout<<*indexValuePointer;
+//     }
+//     else{
+//         cout<<"out of bounds";// only works for input 6;
+//     }
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int arr[5]={1,2,3,4,5};
-    int pos;
-    cin>>pos;
-    pos=pos-1;
-    auto it=arr.begin();
-    std::advance(it, pos);
-    
-
-    return 0;
+    int size=sizeof(arr)/sizeof(int);
+    int userPos;
+    cin>>userPos;
+    if(userPos>0 && userPos<=size){
+      int* end=arr+size;
+      int* indexValuePointer=arr+userPos-1;
+      cout<<*indexValuePointer<<endl;
+    }
+    else{
+        cout<<"out of bounds";
+    }
 }
