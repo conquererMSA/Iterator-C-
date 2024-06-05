5372f4b83a251a4f00516aca1aedf649c963eb17
asus test <msa-99@outlook.com>
2024-06-05 23:20:42 +0600

    printString Iteration

--- printArrayByIterator.cpp
+++ printArrayByIterator.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main(){
     int arr[5]={1,2,3,4,5};
     int* it=arr;
+    cout<<*it<<endl;
     //ekhane it hocce (int arr) er first element er memory address/pointer;
     int* end=arr+5;
-    //ekhane end hocce (int arr) er last element er memory addres/pointer;
+    cout<<*(end-1)<<endl;//5
+    //ekhane end hocce (int arr) er last element er porer ghor (garbage vlue) er memory addres/pointer;
     while(it!=end){
         cout<<*it<<" ";
         // it arr er element gulur memory address hold kore rakhe, it theke value pete hole it ke dereference korte hobe.
